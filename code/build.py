a7315cf6dd381df426dcd8e2955e1c736d00d802
bisheshank <bisheshank@brown.edu>
2024-05-04 01:35:13 -0400

    updated the chord generation

--- code/build.py
+++ code/build.py
@@ -1,126 +1,248 @@
-import re
+import tensorflow as tf
+from transformers import TFBertModel, BertTokenizer
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Input, Dense, Dropout
+
 import os
-import numpy as np
+from utils import re
 import tensorflow as tf
 from tensorflow.keras.layers.experimental.preprocessing import TextVectorization
 
+def preprocess_song(file_path):
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
 
-class ChordLyricProcessor:
-    def __init__(self, song_files_directory="../data/chord-lyric-text/", max_sequence_length_lyrics=250, max_sequence_length_chords=250):
-        self.song_files_directory = song_files_directory
+    # Regular expressions to identify chords and tabs
+    chord_regex = re.compile(r"^[A-G][#b]?(7|5|M|maj7|maj|M7|mmaj7|min7|m7|m|min|dim|dim7|aug|\+|sus2|sus4|7sus2|7sus4)?(add)?[0-9]*/?[A-G]?[#b]?$")
+    section_regex = re.compile(r"^(\(|\[)?[\#]?(chorus|Chorus|CHORUS|verse|Verse|VERSE|intro|Intro|INTRO|outro|Outro|OUTRO|bridge|Bridge|BRIDGE|interlude|Interlude|INTERLUDE|instrumental|Instrumental|INSTRUMENTAL|solo|Solo|SOLO)*( )?[0-9]*(\:|\.)?(\]|\))?$")
+    accidental_regex = re.compile(r"^\w[#b]")
 
-        self.MAX_SEQUENCE_LENGTH_LYRICS = max_sequence_length_lyrics
-        self.MAX_SEQUENCE_LENGTH_CHORDS = max_sequence_length_chords
+    current_section = None
+    key_to_pitch = {
+        'A': 0,
+        'A#': 1,
+        'Bb': 1,
+        'B': 2,
+        'Cb': 2,
+        'B#': 3,
+        'C': 3,
+        'C#': 4,
+        'Db': 4,
+        'D': 5,
+        'D#': 6,
+        'Eb': 6,
+        'E': 7,
+        'Fb': 7,
+        'E#': 8,
+        'F': 8,
+        'F#': 9,
+        'Gb': 9,
+        'G': 10,
+        'G#': 11,
+        'Ab': 11
+    }
+    qual_to_num = {
+        '': 0,
+        '5': 0,
+        'M' : 0,
+        'maj7': 1,
+        'maj': 1,
+        'M7': 1,
+        '7': 2,
+        'm': 3,
+        'mmaj7': 3,
+        'min7': 4,
+        'm7': 4,
+        'dim': 5,
+        'dim7':5,
+        '+': 6,
+        '+5': 6,
+        'aug': 6,
+        'sus2': 7,
+        '7sus2': 7,
+        '2': 7,
+        'sus4': 8,
+        '7sus4': 8,
+        '4': 8
+    }
 
-        self.key_to_pitch = {
-            'A': 0, 'A#': 1, 'Bb': 1, 'B': 2, 'Cb': 2, 'B#': 3, 'C': 3, 'C#': 4, 'Db': 4,
-            'D': 5, 'D#': 6, 'Eb': 6, 'E': 7, 'Fb': 7, 'E#': 8, 'F': 8, 'F#': 9, 'Gb': 9,
-            'G': 10, 'G#': 11, 'Ab': 11
-        }
-        self.qual_to_num = {
-            '': 0, '5': 0, 'M': 0, 'maj7': 1, 'maj': 1, 'M7': 1, '7': 2, 'm': 3, 'mmaj7': 3,
-            'min7': 4, 'm7': 4, 'dim': 5, 'dim7': 5, '+': 6, '+5': 6, 'aug': 6,
-            'sus2': 7, '7sus2': 7, '2': 7, 'sus4': 8, '7sus4': 8, '4': 8
-        }
-
-        self.accidental_regex = re.compile(r"^\w[#b]$")
-        self.section_regex = re.compile(r"^\[.*\]$")
-        self.chord_regex = re.compile(r"^[A-G][#b]?[0-9]?[a-zA-Z0-9#]*$")
-
-        self.lyric_tokenizer = TextVectorization(output_mode='int', output_sequence_length=self.MAX_SEQUENCE_LENGTH_LYRICS)
-
-        self.preprocessed_pairs = []
-
-    def preprocess_song(self, file_path):
-        def get_root_pitch(chord):
-            root_note = chord[0] + chord[1] if self.accidental_regex.match(chord) else chord[0]
-            return self.key_to_pitch[root_note]
-
-        def get_rel_pitch(pitch, key_pitch):
-            return (pitch - key_pitch) % 12
-
-        def chord_to_vector(chord, key_pitch):
-            base, *qual = chord.split('/')
-            root_pitch = get_rel_pitch(get_root_pitch(base), key_pitch)
-            qual = qual[0] if qual else ''
-            base_pitch = root_pitch if not qual else get_rel_pitch(get_root_pitch(qual), key_pitch)
-            qual_number = self.qual_to_num[re.sub(r'\d+', '', qual.split('add')[0])]
-            return [root_pitch, base_pitch, qual_number]
-
-        verses = []
-        with open(file_path, 'r') as file:
-            lines = file.readlines()
-
-        key = lines[0].strip().split(' ')[1]
-        key_pitch = get_root_pitch(key)
-        versenum = -1
-
-        # Initialize the first verse if no sections are provided
-        verses.append({'text': "", 'chords': []})
-        versenum = 0
-
-        for line in lines[1:]:
-            clean_line = re.sub(r"[|\(\)\-%\t\\/*@,x\d]", " ", line).strip()
-            if self.section_regex.match(clean_line):
-                verses.append({'text': "", 'chords': []})
-                versenum = len(verses) - 1
-            else:
-                is_chord_line = all(self.chord_regex.match(word) for word in clean_line.split())
-                if is_chord_line and versenum >= 0:
-                    verses[versenum]['chords'] += [chord_to_vector(word, key_pitch) for word in clean_line.split()]
-                else:
-                    verses[versenum]['text'] += clean_line + " "
-
-        return [verse for verse in verses if verse['chords'] or verse['text']]
-
-    def preprocess_directory(self, directory_path):
-        file_name_regex = re.compile(r"^([A-R]|[a-r])")
-        for file_name in os.listdir(directory_path):
-            if file_name.endswith(".txt") and file_name_regex.match(file_name):
-                file_path = os.path.join(directory_path, file_name)
-                song_data = self.preprocess_song(file_path)
-                self.preprocessed_pairs.extend(song_data)
-
-    def tokenize_lyrics(self, text):
-        return self.lyric_tokenizer(text) if text else np.zeros((self.MAX_SEQUENCE_LENGTH_LYRICS,), dtype=np.int32)
-
-    def pad_chords(self, chords, max_len=None):
-        max_len = max_len or self.MAX_SEQUENCE_LENGTH_CHORDS
-        if chords:
-            padded_chords = tf.keras.preprocessing.sequence.pad_sequences([chords], maxlen=max_len, padding='post', dtype='int32', value=[0, 0, 0])
-            return padded_chords[0]
+    def get_root_pitch(chord):
+        if accidental_regex.match(chord):
+            root_note = chord[0]+chord[1]
         else:
-            return np.zeros((max_len, 3), dtype=np.int32)
+            root_note = chord[0]
+        
+        # print(f"root_note: {root_note}")
 
-    def prepare_dataset(self, batch_size=32):
-        def generator():
-            for item in self.preprocessed_pairs:
-                if item['text'] and item['chords']:
-                    tokenized_lyrics = self.tokenize_lyrics(item['text'])
-                    padded_chords = self.pad_chords(item['chords'])
-                    yield (tokenized_lyrics.numpy(), padded_chords)
-                else:
-                    yield (np.zeros((self.MAX_SEQUENCE_LENGTH_LYRICS,), dtype=np.int32),
-                           np.zeros((self.MAX_SEQUENCE_LENGTH_CHORDS, 3), dtype=np.int32))
+        return key_to_pitch[root_note]
 
-        dataset = tf.data.Dataset.from_generator(
-            generator,
-            output_types=(tf.int32, tf.int32),
-            output_shapes=((self.MAX_SEQUENCE_LENGTH_LYRICS,), (self.MAX_SEQUENCE_LENGTH_CHORDS, 3))
-        )
-        return dataset.batch(batch_size).prefetch(tf.data.AUTOTUNE)
+    verses = []
+    key = lines[0].strip().split(' ')[1]
+    # print(f"key: {key}")
+    key_pitch = get_root_pitch(key)
+    lines = lines[1:]
+    versenum = -1
+    isbreak = 0
 
-    def get_dataset(self):
-        self.preprocess_directory(self.song_files_directory)
-        self.lyric_tokenizer.adapt([item['text'] for item in self.preprocessed_pairs if item['text']])
-        return self.prepare_dataset()
+    def get_rel_pitch(pitch):
+        if pitch >= key_pitch:
+            chord_rel_pitch = pitch - key_pitch
+        else:
+            chord_rel_pitch = 12 + pitch - key_pitch
+        
+        return chord_rel_pitch
 
+    def chord_to_vector(chord):
+        chord_rel_pitch = get_rel_pitch(get_root_pitch(chord))
+        
+        base_split = chord.split('/')
+        if len(base_split) == 2:
+            base_rel_pitch = get_rel_pitch(get_root_pitch(base_split[1]))
+            chord = base_split[0]
+        else:
+            base_rel_pitch = chord_rel_pitch
 
-processor = ChordLyricProcessor()
-dataset = processor.get_dataset()
+        if accidental_regex.match(chord):
+            qual = chord[2:]
+        else:
+            qual = chord[1:]
+        
+        if 'add' in qual:
+            qual = qual[0:qual.index('add')]
+        elif '6' in qual:
+            qual = qual[0:qual.index('6')]
+        elif '9' in qual:
+            qual = qual[0:qual.index('9')]
+        elif '11' in qual:
+            qual = qual[0:qual.index('11')]
+        elif '13' in qual:
+            qual = qual[0:qual.index('13')]
+        elif '15' in qual:
+            qual = qual[0:qual.index('15')]
+        elif '17' in qual:
+            qual = qual[0:qual.index('17')]
 
-for lyrics, chords in dataset.take(1):
-    print("Lyrics batch shape:", lyrics.shape)
-    print("Chords batch shape:", chords.shape)
-    print("Sample lyrics:", lyrics[4])
-    print("Sample chords:", chords[6])
+        # print(f"root: {chord_rel_pitch}")
+        # print(f"base: {base_rel_pitch}")
+        # print(f"qual: {qual}")
+
+        return [chord_rel_pitch, base_rel_pitch, qual_to_num[qual]]
+
+    def get_chord_list(line):
+        line = line.strip()
+        chord_list = line.split(" ")
+        chord_list = [x for x in chord_list if x]
+        # print(f"chord list: {chord_list}")
+        chord_list = [chord_to_vector(x) for x in chord_list]
+        return chord_list
+
+    for line in lines:
+        line = line.strip().replace('|',' ').replace('(',' ').replace(')',' ').replace('-',' ').replace('%',' ').replace('\t',' ').replace('\\','/').replace('/ ',' ').replace('*',' ').replace('@',' ').replace(',  ','   ').replace('x2',' ').replace('x3',' ').replace('x4',' ').replace('x5',' ').replace('x6',' ').replace('x7',' ').replace('x8',' ')
+        # print(f"line: {line}")
+        if section_regex.match(line):
+            # print(f"section regex match; isbreak = {isbreak}")
+            if isbreak == 0:
+                isbreak = 1
+                versenum += 1
+                # print(f"versenum: {versenum}")
+                verses.append({'text': "", 'chords': []})
+        else:
+            # print(f"No section regex match; isbreak = {isbreak}")
+            if isbreak == 1:
+                isbreak = 0
+            is_chord_line = True
+            for word in line.split():
+                if not(is_chord_line and chord_regex.match(word)):
+                    is_chord_line = False
+            if is_chord_line:
+                verses[versenum]['chords'] += get_chord_list(line)
+                # print(verses[versenum]['chords'])
+            else:
+                verses[versenum]['text'] += line + " "
+
+    # FORMAT OF THE OUTPUT:
+    # list (for each song) of dictionaries (for each verse) with keys 'text' and 'chords'
+    # chords are in the format [0-11 (root pitch), 0-11 (base pitch), 0-7 (quality)]
+    return [x for x in verses if x['chords']]
+
+Path = "../data/chord-lyric-text/"
+filelist = os.listdir(Path)
+preprocessed_pairs = []
+file_name = re.compile(r"^([A-R]|[a-r])")
+for i in filelist:
+    if i.endswith(".txt"):
+        # print(i)
+        song_data = preprocess_song(Path + i)
+        # print(song_data)
+        preprocessed_pairs += song_data
+print(preprocessed_pairs[0])
+
+# Load tokenizer and base model
+tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+bert_model = TFBertModel.from_pretrained('bert-base-uncased')
+
+# Encoder: leveraging the pre-trained BERT
+input_ids = Input(shape=(None,), dtype=tf.int32, name="input_ids")
+attention_mask = Input(shape=(None,), dtype=tf.int32, name="attention_mask")
+
+encoder_outputs = bert_model(input_ids, attention_mask=attention_mask)[0]
+
+# Decoder: custom dense layers to predict chords
+x = tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(768, return_sequences=True))(encoder_outputs)
+x = Dropout(0.4)(x)
+x = Dense(768, activation='relu')(x)
+outputs = Dense(12 * 12 * 8, activation='softmax')(x)  # 12 root pitches, 12 base pitches, 8 qualities
+
+# Combine into a single model
+model = Model(inputs=[input_ids, attention_mask], outputs=[outputs])
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+# Model summary
+model.summary()
+
+def encode_chord(root_pitch, base_pitch, quality):
+    """ Encode chord components into a single integer. """
+    return root_pitch * 96 + base_pitch * 8 + quality
+
+def decode_chord(encoded_chord):
+    """ Decode an encoded chord integer back to its components. """
+    root_pitch = encoded_chord // 96
+    base_pitch = (encoded_chord % 96) // 8
+    quality = encoded_chord % 8
+    return (root_pitch, base_pitch, quality)
+
+# Example to demonstrate encoding and decoding
+root_pitch = 5  # D
+base_pitch = 7  # F
+quality = 2     # '7' in your qual_to_num dictionary
+encoded_chord = encode_chord(root_pitch, base_pitch, quality)
+decoded_chord = decode_chord(encoded_chord)
+
+print("Encoded Chord:", encoded_chord)
+print("Decoded Chord:", decoded_chord)
+
+def encode_chords(chords):
+    """ Convert a list of chord vectors to encoded values. """
+    return [encode_chord(chord[0], chord[1], chord[2]) for chord in chords]
+
+def prepare_data(texts, chord_vectors):
+    """ Prepare data for model input from texts and chord vectors. """
+    input_ids, attention_masks, targets = [], [], []
+
+    for text, chords in zip(texts, chord_vectors):
+        inputs = tokenizer(text, return_tensors="tf", padding=True, truncation=True, max_length=512)
+        input_ids.append(inputs['input_ids'])
+        attention_masks.append(inputs['attention_mask'])
+        encoded_chords = encode_chords(chords)
+        targets.append(tf.convert_to_tensor(encoded_chords, dtype=tf.int32))
+
+    return tf.concat(input_ids, axis=0), tf.concat(attention_masks, axis=0), tf.concat(targets, axis=0)
+
+# Assuming texts and chord_vectors are already defined
+texts = ["your lyrics here"]
+chord_vectors = [[[5, 7, 2]]]  # List of lists of chord vectors
+input_ids, attention_masks, targets = prepare_data(texts, chord_vectors)
+
+# Fit model
+model.fit([input_ids, attention_masks], targets, epochs=5, batch_size=32)
+
---
+++ code/chordgeneration/config.py
@@ -0,0 +1,17 @@
+DATASET_PATH = "../../data/wikifonia-synthzone"
+CORPUS_PATH = "corpus.bin"
+WEIGHTS_PATH = 'weights.keras'
+INPUTS_PATH = "inputs"
+OUTPUTS_PATH = "outputs"
+
+EXTENSION = ['.musicxml', '.xml', '.mxl', '.midi', '.mid']
+
+VAL_RATIO = 0.1
+RNN_SIZE = 128
+NUM_LAYERS = 2
+BATCH_SIZE = 512
+EPOCHS = 7
+
+TEMPERATURE = 0
+RHYTHM_DENSITY = 0
+LEAD_SHEET = True
