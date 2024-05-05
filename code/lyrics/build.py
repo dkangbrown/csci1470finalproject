ca476d4588bfcf792769121677dddac0591fe773
bisheshank <bisheshank@brown.edu>
2024-05-04 23:25:12 -0400

    updates to the model

--- code/lyrics/build.py
+++ code/lyrics/build.py
@@ -4,7 +4,11 @@ from model import LyricsGenerator
 
 def main(retrain):
     generator = LyricsGenerator(retrain=retrain)
-    seed_text = "Never gonna give you up"
+
+    if not args.text:
+        seed_text = "Never gonna give you up"
+    else:
+        seed_text = args.text
 
     # Generate different diversity levels
     for diversity in [0.2, 0.5, 1.0]:
@@ -14,5 +18,6 @@ def main(retrain):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Manage Lyrics Generator training and prediction.")
     parser.add_argument("--retrain", action="store_true", help="Set this flag to retrain the model.")
+    parser.add_argument("--text", type=str, help="Seed text to generate lyrics.")
     args = parser.parse_args()
     main(args.retrain)
